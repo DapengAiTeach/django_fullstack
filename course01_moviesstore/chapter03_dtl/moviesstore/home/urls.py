15024e64cf9c6f3058f7343516de38aa8ff51e1b
张大鹏 <1156956636@q.com>
2026-01-03 22:00:18 +0800

    公共顶部导航和底部

--- course01_moviesstore/chapter03_dtl/moviesstore/home/urls.py
+++ course01_moviesstore/chapter03_dtl/moviesstore/home/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'home'
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about.html/', views.about, name='home'),
+    path('about/', views.about, name='about'),
 ]
