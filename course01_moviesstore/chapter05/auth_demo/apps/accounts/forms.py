7c86c8c88484d8dc9fedefc099797989470ab357
张大鹏 <1156956636@q.com>
2026-01-09 17:40:12 +0800

    基本注册功能

---
+++ course01_moviesstore/chapter05/auth_demo/apps/accounts/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class RegisterForm(UserCreationForm):
+    username = forms.CharField(
+        label="账号",
+        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "请输入账号"}),
+    )
+    password1 = forms.CharField(
+        label="密码",
+        widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "请输入密码"}),
+    )
+    password2 = forms.CharField(
+        label="确认密码",
+        widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "请再次输入密码"}),
+    )
+
+    class Meta:
+        model = User
+        fields = ("username", "password1", "password2")
